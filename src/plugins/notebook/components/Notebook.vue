9c51676b5ccaf9eb6392d420a8785b0c9f4d66f6
Nikhil Mandlik <nikhil.k.mandlik@nasa.gov>
2020-08-10 14:51:23 -0700

    lint fixes

--- src/plugins/notebook/components/Notebook.vue
+++ src/plugins/notebook/components/Notebook.vue
@@ -243,7 +243,7 @@ export default {
                 notebookMeta,
                 page,
                 section
-            }
+            };
         },
         dragOver(event) {
             event.preventDefault();
@@ -320,7 +320,10 @@ export default {
         },
         getLinktoNotebook() {
             const objectPath = this.openmct.router.path;
-            const link = objectLink.computed.objectLink.call({ objectPath, openmct: this.openmct });
+            const link = objectLink.computed.objectLink.call({
+                objectPath,
+                openmct: this.openmct
+            });
 
             const selectedSection = this.selectedSection;
             const selectedPage = this.selectedPage;
